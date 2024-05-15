fd27b384cb70081d10386a871e628b6c220b11d8
Vincent Paulin <ecniv81@gmail.com>
2024-05-15 22:21:11 +0200

    feat: login visual form

--- lib/screens/login/login_view.dart
+++ lib/screens/login/login_view.dart
@@ -1,63 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:soigne_moi_mobile/screens/login/login.dart';
+import 'package:soigne_moi_mobile/widgets/auth_widget.dart';
+import 'login.dart';
 
 class LoginView extends StatelessWidget {
   final LoginController controller;
-  const LoginView({super.key, required this.controller});
+  const LoginView({Key? key, required this.controller}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Logo
-            Image.asset(
-              'assets/img/logo.png',
-              height: 100,
-              width: 100,
-            ),
-            const SizedBox(height: 20),
-            // Titre
-            Text(
-              'Soigne Moi Docteur',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Nom',
-                      border: OutlineInputBorder(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    // Logo
+                    Image.asset(
+                      'assets/img/logo.png',
+                      height: 100,
+                      width: 100,
                     ),
-                  ),
-                  SizedBox(height: 10), // Espacement entre les champs
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
+                    const SizedBox(height: 20),
+                    // Titre
+                    Text(
+                      'Soigne Moi Docteur',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  SizedBox(height: 10), // Espacement entre les champs
-                  ElevatedButton(
-                    onPressed: () {
-                      // Ajoutez votre logique de traitement du formulaire ici
-                    },
-                    child: Text('Envoyer'),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Column(
+                  children: [
+                    // Email TextField
+                    buildLoginEmailTextField(controller),
+                    const SizedBox(height: 10),
+                    // Password TextField
+                    buildLoginPasswordTextField(controller),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        'Mot de passe oublié ?',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                // Login Button
+                buildLoginButton(controller),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
