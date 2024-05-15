359eaff8ed61f2208525688439a578deb3878958
Vincent Paulin <ecniv81@gmail.com>
2024-05-15 23:11:20 +0200

    feat: logout in app bar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,10 +1,13 @@
-// widgets/custom_app_bar.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:go_router/go_router.dart';
 
 import 'custom_avatar.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({Key? key});
+  const CustomAppBar({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +15,27 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       forceMaterialTransparency: true,
       title: Row(
         children: [
-          CustomAvatar(
-            sex: 'homme',
+          PopupMenuButton(
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuEntry>[
+                PopupMenuItem(
+                  child: ListTile(
+                    leading: const Icon(Icons.logout),
+                    title: Text('Se déconnecter'),
+                    onTap: () async {
+                      final FlutterSecureStorage secureStorage =
+                          FlutterSecureStorage();
+                      await secureStorage.delete(key: 'access_token');
+                      // Navigate to the login page
+                      if (context.mounted) context.go('/');
+                    },
+                  ),
+                )
+              ];
+            },
+            icon: CustomAvatar(
+              sex: 'homme',
+            ),
           ),
           const SizedBox(
             width: 2.0,
