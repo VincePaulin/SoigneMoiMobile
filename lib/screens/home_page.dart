34fc505bedbc17a0d994dd2745ce5e4e8708aee3
Vincent Paulin <ecniv81@gmail.com>
2024-05-15 17:49:08 +0200

    feat: date today format

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,51 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Importez la bibliothèque intl
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Format de la date
+    String formattedDate = DateFormat('dd/MM/yyyy').format(DateTime.now());
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Row(
-            children: [
+            children: const [
               Text('Nom du docteur'),
               Spacer(),
               //Icon(Icons.logo),
             ],
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Date d\'aujourd\'hui: ${DateTime.now().toString().split(' ')[0]}',
-                style: TextStyle(fontSize: 18),
+            Positioned.fill(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(120),
+                    topRight: Radius.circular(120),
+                  ),
+                ),
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 4, // Raising for shadow effect
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    child: ListTile(
-                      title: Text('Titre de la tuile $index'),
-                      subtitle: Text('Sous-titre de la tuile $index'),
-                      leading: const Icon(Icons.circle),
-                      trailing: const Icon(Icons.arrow_forward_ios),
-                      onTap: () {
-                        // Action to be taken when the ListTile is clicked
-                      },
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Aujourd'hui",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          formattedDate,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
-                  );
-                },
-              ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation: 4, // Raising for shadow effect
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 8, horizontal: 16),
+                        child: ListTile(
+                          title: Text('Titre de la tuile $index'),
+                          subtitle: Text('Sous-titre de la tuile $index'),
+                          leading: const Icon(Icons.circle),
+                          trailing: const Icon(Icons.arrow_forward_ios),
+                          onTap: () {
+                            // Action to be taken when the ListTile is clicked
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ],
         ),
