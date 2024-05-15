83088b757bdc951d1ff5c7d7868eb7442dde941b
Vincent Paulin <ecniv81@gmail.com>
2024-05-15 18:05:25 +0200

    feat: app barr logo

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,16 +2,31 @@
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      forceMaterialTransparency: true,
       title: Row(
-        children: const [
-          Text('Nom du docteur'),
-          Spacer(),
-          //Icon(Icons.logo),
+        children: [
+          Text(
+            'Dr. Prenom Nom',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          const Spacer(),
+          CircleAvatar(
+            radius: kToolbarHeight *
+                0.4, // CircleAvatar size adapted to AppBar height
+            backgroundColor: Colors.transparent,
+            child: Image.asset(
+              'assets/img/logo.png',
+              fit: BoxFit.cover,
+            ),
+          ),
         ],
       ),
     );
