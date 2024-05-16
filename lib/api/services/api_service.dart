1a4c0c660bee6e41fb77427215bb07bfaa175472
Vincent Paulin <ecniv81@gmail.com>
2024-05-16 11:17:30 +0200

    feat: function to recup data from api

--- lib/api/services/api_service.dart
+++ lib/api/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:soigne_moi_mobile/api/services/endpoints.dart';
 
 class Api {
   final storage = FlutterSecureStorage();
@@ -17,5 +18,24 @@ class Api {
   // How to retrieve the token
   Future<String> _getToken() async {
     return await storage.read(key: 'access_token') ?? '';
+  }
+
+  Future<Map<String, dynamic>> fetchDoctorAgenda() async {
+    dio.options.baseUrl = Endpoints.baseUrl;
+
+    try {
+      final response = await dio.get(
+        '/doctor/get-data',
+        options: await _generateOptions(),
+      );
+
+      final data = response.data;
+
+      return data;
+    } on DioException catch (e) {
+      final errorMessage =
+          e.response?.data['error'] ?? e.response?.data['message'];
+      throw errorMessage;
+    }
   }
 }
